361d4294c221c848ba99db3d284ce1c4296ad781
Ksr0201 <kuldeeprathore0201@gmail.com>
2024-12-04 10:33:28 +0530

    removeelement code

---
+++ removeElement.py
@@ -0,0 +1,11 @@
+def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        nums2  = nums[:] 
+        for i in nums2:
+            print(nums)
+            if i == val:
+                nums.remove(i)
+            else:
+                count += 1
+        print(count)
+
